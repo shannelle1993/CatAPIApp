e82638a4869db7f3ab616b954ec8ba514155ba4c
Shannelle Counsell <67432903+shannelle1993@users.noreply.github.com>
2021-08-18 02:40:42 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+const catResult = document.getElementById("cat-result");
+const catButton = document.getElementById("catButton");
+const multiCatsButton = document.getElementById("multiCatsButton");
+const catQty = document.getElementById("number");
+
+catButton.addEventListener("click", getRandomCat);
+multiCatsButton.addEventListener("click", getNumberOfCats);
+
+function getRandomCat() {
+  fetch("https://cataas.com/cat?json=true")
+    .then((res) => res.json())
+    .then((data) => {
+      catResult.innerHTML = `<img src="https://cataas.com/${data.url}"/>`;
+    });
+}
+
+function getNumberOfCats() {
+   fetch(`https://cataas.com/api/cats?limit=${catQty.value}`)
+   .then((res) => res.json())
+   .then((data) => {
+    let images = data.map(cat => `<img src="https://cataas.com/cat/${cat.id}"/>`);
+
+     catResult.innerHTML = images;
+   })};
+
+function getRandomCat() {
+    fetch("https://cataas.com/cat?json=true")
+        .then((res) => res.json())
+        .then((data) => {
+            catResult.innerHTML = `<img src="https://cataas.com/${data.url}"/>`;
+        });
+    }
